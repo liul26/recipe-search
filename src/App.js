46e08c3154eb22b9345e5604304f1441c2b14456
unknown <liul26@uw.edu>
2020-02-19 22:18:06 -0800

    beginning the final project!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { Input } from 'antd';
+import { Spin } from 'antd';
+import Sandwich from './Sandwich.PNG';
+
+const { Search } = Input;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [text, setText] = useState('')
+  const [recipes, setRecipes] = useState([])
+  const [loading, setLoading] = useState(false)
+  
+  async function getRecipes(){
+    setLoading(true)
+    const key='AitgzNZBJNonm2fIFf7fc6EsGIuRfS0a'
+    let url = 'https://api.giphy.com/v1/gifs/search?'
+    url += 'api_key=' + key
+    url += '&q=' + text + '&limit=50&offset=0&rating=G&lang=en'
+    const r = await fetch(url)
+    const body = await r.json()
+    setRecipes(body.data)
+    setText('')
+    setLoading(false)
+  }
+
+  console.log(recipes)
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="title-wrap">Recipe Search</div>
+          <div className="input-wrap">
+            <Search
+              placeholder="Add your ingredients here..."
+              enterButton="Search"
+              size="large"
+              onSearch={value => console.log(value)}
+            />
+          </div>
+        </header>
+        <div className="recipes">
+          {/* {recipes.map((recipe, i)=> <Recipe key={i} {...recipe} />)} */}
+          <div className="loading-wrap">
+            <Spin />
+          </div>
+          <img src={Sandwich} alt="a sandwich"/>
+        </div>
+      </div>
+    );
 }
 
 export default App;
