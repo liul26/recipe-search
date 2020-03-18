a7a60c9fcac26f0dd3a67c786517b60494ae4ea6
unknown <liul26@uw.edu>
2020-03-17 22:00:55 -0700

    show favorites toggle implemented

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Sandwich from './Sandwich.PNG';
 import { Input } from 'antd';
 import { Spin } from 'antd';
 import { Card } from 'antd';
-import { StarOutlined } from '@ant-design/icons';
+import { StarOutlined, StarFilled } from '@ant-design/icons';
 import { Button, Tooltip } from 'antd';
 const { Meta } = Card;
 const { Search } = Input;
-
+ 
 function App() {
   const [searchterm, setSearchterm] = useState('')
   const [recipes, setRecipes] = useState([])
@@ -16,13 +16,21 @@ function App() {
   const [prevsearch, setprevSearch] = useState('')
   const [init, setInit] = useState(false)
   const [myfaves, setMyFaves] = useState([])
-
+  const [showfaves, setShowFaves] = useState(false)
+ 
   function getFaved () {
-    let faves = []
-    if (localStorage && localStorage.getItem('faved')) {
-      faves = JSON.parse(localStorage.getItem('faved'))
+    if (showfaves === false) {
+      let faves = []
+      if (localStorage && localStorage.getItem('faved')) {
+        faves = JSON.parse(localStorage.getItem('faved'))
+      }
+      setMyFaves(faves)
+      setShowFaves(true)
+    } else {
+      setShowFaves(false);
+      setMyFaves([]);
     }
-    setMyFaves(faves)
+    //add show no show toggle here?
   }
 
   async function getRecipes(){
@@ -50,7 +58,7 @@ function App() {
     console.log(recipes)
     setInit(true)
   }
-
+ 
     return (
       <div className="App">
         <header className="App-header">
@@ -63,97 +71,101 @@ function App() {
               value={searchterm} /*IMPORTANT*/
               onChange={e=> setSearchterm(e.target.value)} /*IMPORTANT*/
               // onKeyPress={e=> {if(e.key==='Enter') getRecipes()}}
-              onSearch={value => console.log(value)}
               // onClick={e=> getRecipes()}
+              onSearch={value => console.log(value)}
               onSearch={getRecipes}
-              //????^
             />
           </div>
           <div>
             <Tooltip title="favorite">
-                <Button 
-                  type="primary" 
+                <Button
+                  type="primary"
                   onClick={getFaved}
-
                 >
                   Show favorite recipes
                 </Button>
             </Tooltip>
           </div>
-        
         </header>
-
+ 
         <div className="body">
           <div className="loading-wrap">
-            { loading && <Spin /> } 
+            { loading && <Spin /> }
           </div>
           {init && <div className="text-wrap">
             You searched for: <div style={{fontWeight: 'bold'}}>{prevsearch}</div>
             {console.log(prevsearch)}
           </div>}
           {!init && <div className="text-wrap">Start searching!</div>}
-          <div className="favorites">
-            {myfaves.map((recipe, i)=> {console.log(recipe); return <Recipe key={`favorite-${i}`} recipe={recipe}/>})}
-          </div>
+          {showfaves &&
+            <div className="favorites">
+              {myfaves.map((recipe, i)=> {console.log(recipe); return <Recipe key={`favorite-${i}`} recipe={recipe} faved={true} />})}
+            </div>
+          }
+          {!showfaves && <div className="text-wrap"></div>}
           <div className="results">
             {recipes.map((recipe, i)=> <Recipe key={i} {...recipe} />)}
           </div>
           {recipes.length===0 && init && <div className="text-wrap">Couldn't find any recipes...check your spelling and try again.</div>}
-          <div className="image-wrap">
-            <img src={Sandwich} alt="a sandwich"/> {/*if data showing, hide sandwich. no data. show sandwich*/}
-          </div>
-          <div className="text-wrap">
-            Made with love by Leanne Liu in React | Copyright 2020
-          </div>
-        </div>
-      </div>
-    );
+         <div className="image-wrap">
+           <img src={Sandwich} alt="a sandwich"/> {/*if data showing, hide sandwich. no data. show sandwich*/}
+         </div>
+         <div className="text-wrap">
+           Made with love by Leanne Liu in React | Copyright 2020
+         </div>
+       </div>
+     </div>
+   );
 }
-
+ 
 function Recipe(props){
-  console.log(props)
-  const imageurl = props.recipe.image //props accounts for recipe.hits already
-  const source = props.recipe.url
-  const label = props.recipe.label
-  const webname = props.recipe.source
-
-  //have a function that saves faves to localstorage
-  function favorite() {
-    let favoriteRecipes = []
-    if (localStorage && localStorage.getItem('faved')) {
-      favoriteRecipes = JSON.parse(localStorage.getItem('faved'))
-    }
-    let existing=favoriteRecipes.filter(x=>x.url===props.recipe.url)
-    if (existing.length <= 0) {
-      favoriteRecipes.push(props.recipe)
-    } else {
-      let index = favoriteRecipes.findIndex(x=>x.url===props.recipe.url)
-      favoriteRecipes.splice(index, 1)
-    }
-    localStorage.setItem('faved', JSON.stringify(favoriteRecipes))
-  }
-
-  return (
-    <div className="recipe">
-     {/* images.fixed_height. <-- also ok to take out below */}
-      <Card
-          hoverable
-          style={{ width: 300 }}
-          cover={<img alt="recipe photo" src= {imageurl} />}
-          onClick={()=>window.open(source, '_blank')}
-      >
-      <Meta title={label} description={webname} />
-      <Tooltip title="Favorite this recipe">
-        <Button 
-          icon={<StarOutlined />} 
-          onClick={favorite} 
-          style={{ marginTop: 20 }}
-          onClick={e=>e.stopPropagation()}
-          //???^^^
-        >Add to favorites</Button>
-      </Tooltip>
-      </Card>
-  </div>)
+ console.log(props)
+ const imageurl = props.recipe.image //props accounts for recipe.hits already
+ const source = props.recipe.url
+ const label = props.recipe.label
+ const webname = props.recipe.source
+ const [isFaved, setIsFaved] = useState(props.faved ? props.faved : false);
+ 
+ //have a function that saves faves to localstorage
+ function favorite(e) {
+   e.stopPropagation();
+   let favoriteRecipes = []
+   if (localStorage && localStorage.getItem('faved')) {
+     favoriteRecipes = JSON.parse(localStorage.getItem('faved'))
+   }
+   let existing=favoriteRecipes.filter(x=>x.url===props.recipe.url)
+   if (existing.length <= 0) {
+     favoriteRecipes.push(props.recipe)
+     setIsFaved(true);
+   } else {
+     let index = favoriteRecipes.findIndex(x=>x.url===props.recipe.url)
+     favoriteRecipes.splice(index, 1)
+     setIsFaved(false);
+   }
+   localStorage.setItem('faved', JSON.stringify(favoriteRecipes))
+ }
+ 
+ return (
+   <div className="recipe">
+    {/* images.fixed_height. <-- also ok to take out below */}
+     <Card
+         hoverable
+         style={{ width: 300 }}
+         cover={<img alt="recipe photo" src= {imageurl} />}
+         onClick={()=>window.open(source, '_blank')}
+     >
+     <Meta title={label} description={webname} />
+     <Tooltip title="Favorite this recipe">
+       <Button
+         icon={isFaved ? <StarFilled /> : <StarOutlined />}
+         onClick={favorite}
+         style={{ marginTop: 20 }}
+         // Moved this into favorite
+         // onClick={e=>e.stopPropagation()}
+       >Add to favorites</Button>
+     </Tooltip>
+     </Card>
+ </div>)
 }
-
+ 
 export default App;
